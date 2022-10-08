8418869b9e4d63b93f274bf31c81acbda3fc960c
John Culver <109003378+johneculver@users.noreply.github.com>
2022-10-08 16:58:00 -0500

    Changed main.js from a generator of singular superhero quotes to a randomizer of subjects, verbs, and objects.

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
-const quotes = [`"I was having twelve percent of a moment." - Pepper Potts`, `"Give me a scotch. I'm starving." - Tony Stark`, `"I can do this all day." - Steve Rogers`, `"This drink… I like it!" - Thor, referring to coffee`, `"Doth mother know you wear-eth her drapes?" - Tony Stark`, `"On your left" - Steve Rogers`, `"The crabby puppy is so cute he makes me wanna die!" - Mantis`, `"Cuttlefish!" - Ulysses Klaue`, `"Baskin-Robbins always finds out." - Scott Lang's Manager`, `"Do you lay eggs?" - Ned`, `"Behold! My stuff." - Skurge`, `"What's the matter with you kids? You never seen a spaceship before?" - Stan Lee`, `"Hand." -King Shark`, `"You should be very proud." - Wonder Woman, upon the discovery of ice cream`];
+const composers = ["Mozart", "Beethoven", "Mahler", "Debussy", "Stranvinsky", "Schumann", "Copland"];
+const verbs = ["leaped over", "leered at", "smelled", "drew", "sniffed", "beguiled", "composed", "conducted", "pranced around"];
+const objects = ["3 delicious hoagies.", "a gaudy mansion.", "a scintillating pond.", "a vibrant meadow.", "an entire apple orchard.", "a roll of parchment.", "the Misty Mountains.", "a large piece of sophisticated machinery."];
 
 const quoteGenerator = () => {
-    let i = Math.floor(Math.random() * 14)
+    let i = Math.floor(Math.random() * 7)
+    let j = Math.floor(Math.random() * 9)
+    let k = Math.floor(Math.random() * 8)
     //console.log(i);
-    return quotes[i];
+    const quote = `${composers[i]} ${verbs[j]} ${objects[k]}`
+    return quote;
 }
 
 console.log(quoteGenerator());
